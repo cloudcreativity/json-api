43501ff098d03b2ec09530b199541721e9ef6548
Christopher Gammie <contact@gammie.co.uk>
2015-08-12 21:31:30 +0100

    Add license

--- src/Decoders/RelationshipDecoder.php
+++ src/Decoders/RelationshipDecoder.php
@@ -1,11 +1,31 @@
 <?php
 
+/**
+ * Copyright 2015 Cloud Creativity Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 namespace CloudCreativity\JsonApi\Decoders;
 
 use CloudCreativity\JsonApi\Error\MultiErrorException;
 use CloudCreativity\JsonApi\Object\Relationships\Relationship;
 use CloudCreativity\JsonApi\Validator\ValidatorAwareTrait;
 
+/**
+ * Class RelationshipDecoder
+ * @package CloudCreativity\JsonApi
+ */
 class RelationshipDecoder extends AbstractDecoder
 {
 
